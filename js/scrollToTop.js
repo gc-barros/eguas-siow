779aa5b682d9123456aeadb6e9c0c029e67f8b67
Gabriel Barros <87779356+gc-barros@users.noreply.github.com>
2021-09-30 11:55:09 -0300

    Botão de voltar ao topo implementado

---
+++ js/scrollToTop.js
@@ -0,0 +1,39 @@
+// Selecionando o botão
+const mybutton = document.getElementById("scroll-button");
+
+// Temporizador do scroll
+let scrollTimer = -1;
+
+// Dispara a função ao scrollar a página
+window.onscroll = function () {   
+    scrollFunction();
+};
+
+function scrollFunction() {
+    // Reseta o temporizador ao scrollar
+    if (scrollTimer != -1) {
+        clearTimeout(scrollTimer);
+    }
+
+    // Esconde o botão após 3000ms, se o scroll parar
+    scrollTimer = window.setTimeout("hideButton()", 3000);
+
+    // Quando o usuário scrollar a página 150px para baixo, exibe o botão
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        mybutton.style.display = "block";
+       
+    } else {
+        hideButton();
+    }
+}
+
+// Esconder o botão
+function hideButton() {
+    mybutton.style.display = "none";
+}
+
+// Quando o usuário clicar no botão, voltará ao topo da página
+function topFunction() {
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, IE e Opera
+}
